f8d24ac7bfce6b9090e30e6fc2f86fd4b1655c41
Logan Kirkland <logan@logankirk.land>
2024-05-01 00:40:03 -0400

    Updated link styling

--- sourcehut_lists.user.js
+++ sourcehut_lists.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sourcehut Lists Line Wrap Fix
 // @namespace    https://git.sr.ht/~logankirkland/sourcehut-lists-userscript/tree/main/item/sourcehut_lists.user.js
-// @version      1.0
+// @version      1.1
 // @description  Fixes line wrap issue on sourcehut lists by adding CSS
 // @author       Logan Kirkland <logan@logankirk.land>
 // @match        https://lists.sr.ht/*
@@ -11,9 +11,14 @@
 (function () {
     'use strict';
 
-    // Add CSS for message body formatting
+    // Add CSS for message body formatting and link styling
     var style = document.createElement('style');
-    style.innerHTML = '.message-body { white-space: pre-wrap; } .message-body.original { white-space: pre; }';
+    style.innerHTML = `
+    .message-body { white-space: pre-wrap; }
+    .message-body.original { white-space: pre; }
+    .original-link { color: #CDC8C2; text-decoration: underline; }
+    .original-link:hover { color: #CDC8C2; text-decoration: none; }
+    `;
     document.head.appendChild(style);
 
     // Function to toggle message body formatting
@@ -27,6 +32,7 @@
         var originalLink = document.createElement('a');
         originalLink.textContent = 'Show original';
         originalLink.href = '#';
+        originalLink.classList.add('original-link');
         originalLink.addEventListener('click', function (event) {
             event.preventDefault();
             var messageHeader = event.target.closest('.message-header');
@@ -36,7 +42,8 @@
                 event.target.textContent = messageBody.classList.contains('original') ? 'Show wrapped' : 'Show original';
             }
         });
-        date.appendChild(document.createTextNode(' '));
+        var separator = document.createTextNode('\xa0·\xa0\xa0');
+        date.appendChild(separator);
         date.appendChild(originalLink);
     });
 })();
