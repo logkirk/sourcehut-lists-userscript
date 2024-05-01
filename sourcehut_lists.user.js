c75db4700bae5cb56dcd4fb0c5de54fd05705ff3
Logan Kirkland <logan@logankirk.land>
2024-04-30 22:12:18 -0400

    Updated namespace

--- sourcehut_lists.user.js
+++ sourcehut_lists.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Sourcehut Lists Line Wrap Fix
-// @namespace    https://sr.ht/~logankirkland/
+// @namespace    https://git.sr.ht/~logankirkland/sourcehut-lists-userscript/tree/main/item/sourcehut_lists.user.js
 // @version      0.1
 // @description  Fixes line wrap issue on sourcehut lists by adding CSS
 // @author       Logan Kirkland <logan@logankirk.land>
